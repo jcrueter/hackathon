08d2f58791b6feef2615b5cad62709f93d2460f2
SarahPAms <Sarah.L.Polan@gmail.com>
2017-03-10 14:55:46 +0100

    new countries scaffolding

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,17 @@
 Developer.delete_all
 Country.delete_all
 
-dev1 = Developer.create!( { first_name: "Sarah", last_name: "Polan", age: 31, trait: "Open minded", skills: "Ruby", image_url: "https://goo.gl/wB6q9q" } )
-dev2 = Developer.create!( { first_name: "First", last_name: "Last", age: 10, trait: "Whatever", skills: "Rails", image_url: "https://goo.gl/wB6q9q" } )
-dev3 = Developer.create!( { first_name: "Michiel", last_name: "Rueter", age: 38, trait: "Cool", skills: "Computers", image_url: "https://goo.gl/wB6q9q" } )
-
 country1 = Country.create!( { name: "the Netherlands"} )
 country2 = Country.create!( { name: "Syria"} )
 country3 = Country.create!( { name: "United States of America"} )
+
+countries_count = Country.all.length
+puts "#{countries_count} countries were created."
+
+
+dev1 = Developer.create!( { first_name: "Sarah", last_name: "Polan", age: 31, trait: "Open minded", skills: "Ruby", image_url: "https://goo.gl/wB6q9q", country: country3 } )
+dev2 = Developer.create!( { first_name: "First", last_name: "Last", age: 10, trait: "Whatever", skills: "Rails", image_url: "https://goo.gl/wB6q9q", country: country2 } )
+dev3 = Developer.create!( { first_name: "Michiel", last_name: "Rueter", age: 38, trait: "Cool", skills: "Computers", image_url: "https://goo.gl/wB6q9q", country: country1 } )
+
+developers_count = Developer.all.length
+puts "#{developers_count} developers were created."
