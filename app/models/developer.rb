a3a16ca4c4756a4f3150120ceebcec28a5ca22a7
jcrueter <michiel@rueter.nl>
2017-03-10 11:53:14 +0100

    Display list of developers

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,2 +1,5 @@
 class Developer < ApplicationRecord
+  def self.order_by_name
+    order(:first_name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "developers#index"
+
+  get "developers" => "developers#index"
+  get "developers" => "developers#create"
+  get "developers" => "developers#edit"
+  get "developers" => "developers#find"
+
 end
