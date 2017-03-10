85c8c98449495da717b3c864821ed16baad90f81
jcrueter <michiel@rueter.nl>
2017-03-10 14:55:16 +0100

    Modified a lot

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -14,4 +14,33 @@ class DevelopersController < ApplicationController
   def show
     @developer = Developer.find(params[:id])
   end
+
+  def update
+    @developer = Developer.find(params[:id])
+
+    if @developer.update_attributes(developer_params)
+      redirect_to @developer
+    else
+      render 'edit'
+    end
+  end
+
+  def create
+
+  @developer = Developer.new(developer_params)
+
+  if @developer.save
+     redirect_to @developer
+  else
+     render 'new'
+  end
+end
+
+
+  private
+
+  def developer_params
+    params.require(:developer).permit(:first_name, :last_name, :age, :trait, :skills, :image_url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
   root "developers#index"
 
+#resources :developers
+
   get "developers" => "developers#index"
   get "developers/new" => "developers#new", as: :new_developer
   get "developers/:id" => "developers#show", as: :developer
   post "developers" => "developers#create"
+  get "developers/:id/edit" => 'developers#edit', as: :edit_developer
 
-  get 'developers/:id/edit' => 'developers#edit', as: :edit_developer
+  patch "developers/:id" => "developers#update"
 
 end
